6c62334d785d830e0c12a825f75bb29591a9986d
Julian Vega <pamilo1@hotmail.com>
2024-11-21 14:19:46 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // error reporting
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $connection = new mysqli('127.0.0.1', 'root', '', 'ajc-forum');
     $connection->set_charset('utf8mb4');
 
